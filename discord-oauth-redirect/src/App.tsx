124b8398e15a97c03effdc17297ee088383e605a
Kajiwara Kensei <admin@kajidog.com>
2024-05-04 01:32:55 +0900

    add oauth2 site

---
+++ discord-oauth-redirect/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
+
+function App() {
+  let [searchParams, setSearchParams] = useSearchParams();
+
+  useEffect(() => {
+    const code = searchParams.get("code");
+    if (code) {
+      // ここでカスタムURLスキームを使ってアプリにコードを送る
+      // 例: yourapp://auth?code=YOUR_AUTH_CODE
+      window.location.href = `voiceping://auth?code=${code}`;
+    }
+  }, [searchParams]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {searchParams.has("code") ? (
+          <p>認証コードをアプリに送信中...</p>
+        ) : (
+          <p>認証コードが見つかりません。</p>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
