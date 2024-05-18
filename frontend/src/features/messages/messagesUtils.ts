acb504bda50d6107ae594eb8ae659bb373147f48
Kajiwara Kensei <admin@kajidog.com>
2024-05-19 00:32:18 +0900

    update: visible guild audio

---
+++ frontend/src/features/messages/messagesUtils.ts
@@ -0,0 +1,12 @@
+import { MessageEvent } from '../../types';
+
+export const createGuildMessageCallback = (
+  guildId: string,
+  callback: (message: MessageEvent) => void
+) => {
+  return (message: MessageEvent) => {
+    if (message.guild_id === guildId) {
+      callback(message);
+    }
+  };
+};
---
+++ frontend/src/types/index.ts
@@ -0,0 +1,8 @@
+export type MessageEvent = {
+  username: string;
+  user_id: string;
+  content: string;
+  id: string;
+  speaker: string;
+  guild_id: string;
+};
