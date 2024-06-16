15f536618832f56340ea428be16e80473ea18d81
Kajiwara Kensei <admin@kajidog.com>
2024-06-16 15:29:33 +0900

    Prevent internal processing during network requests in BotProtect and UserProtect useEffect hooks
    - Added checks to skip internal processing while network requests are ongoing in BotProtect and UserProtect.
    - Updated useEffect hooks to handle this condition.

--- frontend/src/app/accessToken/UserProtect.tsx
+++ frontend/src/app/accessToken/UserProtect.tsx
@@ -4,20 +4,24 @@ import { useEffect, useState } from 'react';
 import { GetDiscordAvatar } from '../../../wailsjs/go/app/App';
 
 export function UserProtect() {
-  const { accessToken, setUserInfo } = useUser();
+  const { accessToken, setUserInfo, handleRefreshAccessToken } = useUser();
   const [loading, setLading] = useState<boolean | null>(false);
   useEffect(() => {
+    if (loading) {
+      return;
+    }
     setLading(true);
     if (accessToken) {
       GetDiscordAvatar(accessToken)
         .then(setUserInfo)
+        .catch(handleRefreshAccessToken)
         .finally(() => {
           setLading(false);
         });
     } else {
       setLading(false);
     }
-  }, []);
+  }, [accessToken]);
   // トークンが設定されてない場合は設定画面へ
   if (!accessToken) {
     return <Navigate to="/bot/token" />;
